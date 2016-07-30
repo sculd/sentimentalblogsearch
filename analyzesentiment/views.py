e74e46dc2b9682167c6d4528623525f029c07159
Jun <hyungjun@amazon.com>
2016-07-30 03:51:22 -0700

    restless

--- analyzesentiment/views.py
+++ analyzesentiment/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader, RequestContext
 
+from restless.views import Endpoint
+
 def index(request):
     return render(request, 'index.html', {})
     #return HttpResponse("Hello, world. You're at the sentiment index.")
