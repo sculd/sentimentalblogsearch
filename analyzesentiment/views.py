5467d3f6bb66735ffda9231dcdb098f6424b145e
Jun <hyungjun@amazon.com>
2016-07-29 23:42:16 -0700

    add view to analyzesentiment

--- analyzesentiment/views.py
+++ analyzesentiment/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader, RequestContext
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+    #return HttpResponse("Hello, world. You're at the sentiment index.")
+
+# REST
+from restless.views import Endpoint
+
+class Hello(Endpoint):
+    def get(self, request):
+        name = request.params.get('name', 'World')
+        return {'message': 'Hello, %s!' % name}
+
+from analyzesentiment.analyze import emotion, emotionUrl
+
+class TextAnalyzer(Endpoint):
+    def get(self, request):
+        text = request.params.get('text', 'seattle rules')
+        res = emotion(text)
+        return res
+
+class UrlAnalyzer(Endpoint):
+    def get(self, request):
+        text = request.params.get('url', 'http://www.yahoo.com')
+        res = emotionUrl(text)
+        return res
+
