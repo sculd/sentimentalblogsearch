89435df7f32398a58a596cdd7fefebcb146db0bc
Jun <hyungjun@amazon.com>
2016-07-30 00:01:19 -0700

    index

--- analyzesentiment/views.py
+++ analyzesentiment/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader, RequestContext
 
 def index(request):
-    #return render(request, 'index.html', {})
-    return HttpResponse("Hello, world. You're at the sentiment index.")
+    return render(request, 'templates/index.html', {})
+    #return HttpResponse("Hello, world. You're at the sentiment index.")
 
 # REST
