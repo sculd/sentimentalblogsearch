4a245f77d6c5aabd9ad41a2b21ee8ac79766564b
Jun <hyungjun@amazon.com>
2016-07-30 03:57:40 -0700

    analyze

---
+++ analyzesentiment/analyze.py
@@ -0,0 +1,50 @@
+import indicoio
+from bs4 import BeautifulSoup
+import nltk   
+import urllib.request
+import logging
+logging.basicConfig(filename='analyze.log',level=logging.INFO)
+logger = logging.getLogger(__name__)
+#indicoio.config.api_key = '912e0cb4f75ec4449e341d535fefa6f4'
+
+def emotion(text):
+    text = text.encode('ascii','ignore')
+    print('[emotion] analyzing text...')
+    logger.info('[emotion] analyzing text:')
+    logger.info(text)
+    res = indicoio.emotion(text)
+    logger.info(res)
+    return res
+
+def emotionUrl(url):
+    url = str(url)
+    print('[emotionUrl] url:', url)
+    logger.info('[emotionUrl] url: ' + url)
+    html_str = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html_str, 'html.parser')
+    raw = soup.get_text()
+    text = ' '.join(raw.split())
+    return emotion(text)
+
+if __name__ == "__main__":
+    # single example
+    res = indicoio.sentiment("I love writing code!")
+    print(res)
+
+    # batch example
+    res = indicoio.sentiment([
+        "I love writing code!",
+        "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
+    ])
+    print(res)
+
+    # single example
+    res = indicoio.emotion("I did it. I got into Grad School. Not just any program, but a GREAT program. :-)")
+    print(res)
+
+    # batch example
+    res = indicoio.emotion([
+        "I did it. I got into Grad School. Not just any program, but a GREAT program. :-)",
+        "Like seriously my life is bleak, I have been unemployed for almost a year."
+    ])
+    print(res)
--- analyzesentiment/views.py
+++ analyzesentiment/views.py
@@ -14,3 +14,18 @@ class Hello(Endpoint):
     def get(self, request):
         name = request.params.get('name', 'World')
         return {'message': 'Hello, %s!' % name}
+
+from analyzesentiment.analyze import emotion, emotionUrl
+
+class TextAnalyzer(Endpoint):
+    def get(self, request):
+        text = request.params.get('text', 'seattle rules')
+        res = emotion(text)
+        return res
+
+class UrlAnalyzer(Endpoint):
+    def get(self, request):
+        text = request.params.get('url', 'http://www.yahoo.com')
+        res = emotionUrl(text)
+        return res
+
